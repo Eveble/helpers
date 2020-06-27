242f9ce86d2ff3c4db56f1d4f1ae3060265a2bb0
Adam Desivi <desivi@desivi.com>
2020-06-27 18:48:32 +0200

    feat: add literal constants

---
+++ src/constants/literal.ts
@@ -0,0 +1,36 @@
+export const NATIVE_ERROR_CLASSES = [
+  Error,
+  EvalError,
+  RangeError,
+  ReferenceError,
+  SyntaxError,
+  TypeError,
+  URIError,
+];
+
+export const NATIVE_TYPES = {
+  // Order is important!
+  string: String,
+  boolean: Boolean,
+  date: Date,
+  number: Number,
+  regexp: RegExp,
+  array: Array,
+  symbol: Symbol,
+  map: Map,
+  function: Function,
+  object: Object,
+};
+
+export const NATIVE_CONSTRUCTOR_NAMES = [
+  'String',
+  'Boolean',
+  'Date',
+  'Number',
+  'RegExp',
+  'Array',
+  'Symbol',
+  'Map',
+  'Function',
+  'Object',
+];
